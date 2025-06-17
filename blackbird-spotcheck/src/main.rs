13b69844a05f3ba058d0252a378f22dad7c9d54f
Philpax <me@philpax.me>
2025-06-18 00:51:58 +0200

    feat(spotcheck): print 10 most listened to albums

--- blackbird-spotcheck/src/main.rs
+++ blackbird-spotcheck/src/main.rs
@@ -11,7 +11,7 @@ fn main() -> anyhow::Result<()> {
     let albums_path = output_dir.join("albums.ndjson");
     let tracks_path = output_dir.join("tracks.ndjson");
 
-    let (_albums, _tracks) = if let Some(spotify_data_path) = spotify_data_path {
+    let (albums, _tracks) = if let Some(spotify_data_path) = spotify_data_path {
         let (albums, tracks) = spotify::parse_and_collate_data(&spotify_data_path)?;
         albums.save(&albums_path)?;
         tracks.save(&tracks_path)?;
@@ -19,6 +19,18 @@ fn main() -> anyhow::Result<()> {
     } else {
         (Albums::load(&albums_path)?, Tracks::load(&tracks_path)?)
     };
+
+    let mut albums_vec = albums.0.values().collect::<Vec<_>>();
+    albums_vec.sort_by_key(|album| -(album.play_count as i32));
+    for (i, album) in albums_vec[..10].iter().enumerate() {
+        println!(
+            "{}: {} - {} ({} plays)",
+            i + 1,
+            album.album_id.artist,
+            album.album_id.album,
+            album.play_count
+        );
+    }
 
     Ok(())
 }
