1bff7905fc3a947e52abe592ef17912736ec23f3
Philpax <me@philpax.me>
2025-09-17 06:38:41 +0200

    fix(ui): tweak cover art constants for better UX at cost of memory

--- blackbird/src/cover_art_cache.rs
+++ blackbird/src/cover_art_cache.rs
@@ -7,9 +7,9 @@ use std::{
 
 use blackbird_core::{CoverArt, Logic};
 
-const TIME_BEFORE_LOAD_ATTEMPT: Duration = Duration::from_millis(200);
-const CACHE_ENTRY_TIMEOUT: Duration = Duration::from_secs(1);
-const MAX_CACHE_SIZE: usize = 15;
+const TIME_BEFORE_LOAD_ATTEMPT: Duration = Duration::from_millis(100);
+const CACHE_ENTRY_TIMEOUT: Duration = Duration::from_secs(5);
+const MAX_CACHE_SIZE: usize = 30;
 
 pub struct CoverArtCache {
     cover_art_loaded_rx: std::sync::mpsc::Receiver<CoverArt>,
